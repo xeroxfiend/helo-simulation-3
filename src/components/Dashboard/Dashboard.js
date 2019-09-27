b20feca416a45dd9ce95a3b232a58fc1f5e38f98
Brian Lowe <mynameisboy@gmail.com>
2019-09-27 16:47:33 -0600

    commit before standups

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,22 +1,32 @@
-import React, {Component} from 'react'
+import React, {Component} from "react";
+import "./dashboard.css";
+import searchButton from "../../assets/searchbutton.png";
 
 class Dashboard extends Component {
-    constructor() {
-        super()
-        this.state = {
+  constructor() {
+    super();
+    this.state = {
+      search: ""
+    };
+  }
 
-        }
-    }
-
-
-    render() {
-        return (
-            <div className="dashboard">
-                Dashboard!
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="dashboard">
+        <div className="search-container">
+          <div className="input-and-buttons">
+            <input className="search-bar" type="text" />
+            <img src={searchButton} alt="search" className="search-button" />
+            <button className="reset">Reset</button>
+          </div>
+          <div className="filter-container">
+              <h3 className="filter-text">My Posts</h3>
+            <input type="checkbox" className="filter" />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-
-export default Dashboard
+export default Dashboard;
