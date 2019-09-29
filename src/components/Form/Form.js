7fd5725ad3af84a9d255242396fa940ba00a161e
Brian Lowe <mynameisboy@gmail.com>
2019-09-28 21:07:02 -0600

    styling

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -24,18 +24,18 @@ class Form extends Component {
             <div className="form">
                 <div className="form-container">
                     <h1 className="new-post">New Post</h1>
-                    <div className="title">
+                    <div className="title-container">
                         <h4 className="title-text">Title:</h4>
                         <input onChange={e => this.handleChange(e.target.value, 'title')} type="text" className="title-input"/>
                     </div>
-                    <img src="" alt="" className="form-image"/>
+                    <img src={this.state.url} alt="" className="form-image"/>
                     <div className="url">
                         <h4 className="url-text">Image URL:</h4>
                         <input onChange={e => this.handleChange(e.target.value, 'url')} type="text" className="url-input"/>
                     </div>
                     <div className="content">
                         <h4 className="content-text">Content:</h4>
-                        <input onChange={e => this.handleChange(e.target.value, 'content')} type="text" className="content-input"/>
+                        <textarea onChange={e => this.handleChange(e.target.value, 'content')} type="text" className="content-input"/>
                     </div>
 
                 </div>
